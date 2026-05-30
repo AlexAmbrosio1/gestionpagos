0d4c033f66242a5c7173ee0c9ada40a9f72f8377
AlexAmbrosio1 <alexsebas79@gmail.com>
2026-05-29 23:41:16 -0500

    Corregir prefijo api en auth service

--- gestionpagosfront/src/app/services/auth.service.ts
+++ gestionpagosfront/src/app/services/auth.service.ts
@@ -21,9 +21,7 @@ import {
 
 export class AuthService {
 
-  private apiUrl =
-  `${environment.apiUrl}/auth`;
-
+ private apiUrl = `${environment.apiUrl}/api/auth`;
   constructor(
     private http: HttpClient
   ) {}
